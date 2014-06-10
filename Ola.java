5a534ee50ce7aca7b3ff78c224fedcb81fb397f2
oswaldolinhares <oswaldolinhares@gmail.com>
2014-06-10 16:15:36 -0300

    Primeiro Commit - Teste

---
+++ Ola.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+public class Ola extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override public void start(Stage stage){
+				Button btn = new Button("Clica em Mim");
+				StackPane raiz = new StackPane();	
+				raiz.getChildren().add(btn);
+				Scene scene = new Scene(raiz);
+				stage.setScene(scene);
+				stage.setWidth(300); stage.setHeight(300);
+        stage.setTitle("Ola Mundo"); stage.show();
+    }
+}
