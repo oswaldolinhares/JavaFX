3f497b0f76545fa950c2f67f19ba39ede7342095
oswaldolinhares <oswaldolinhares@gmail.com>
2014-06-10 21:00:27 -0300

    Atualização do arquivo Ola.java

--- Ola.java
+++ Ola.java
@@ -1,14 +1,24 @@
+/*
+ * By Oswaldo Linhares - oswaldolinhares@gmail.com
+ *
+ * Como todo bom início de aprendizagem 
+ * um Hello World em JavaFX!
+ *
+ */
+
+//IMPORTANDO BIBLIOTECAS
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
+
 public class Ola extends Application {
     public static void main(String[] args) {
         launch(args);
     }
     @Override public void start(Stage stage){
-				Button btn = new Button("Clica em Mim");
+					Button btn = new Button("Clica em Mim"); 
 				StackPane raiz = new StackPane();	
 				raiz.getChildren().add(btn);
 				Scene scene = new Scene(raiz);
